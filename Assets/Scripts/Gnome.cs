fd3db52da58b1128f631dd64948b463800eca1bb
Терентьев Алексей <aateren7@mts.ru>
2018-08-23 08:48:29 +0300

    Начальный коммит

---
+++ Assets/Scripts/Gnome.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gnome : MonoBehaviour
+{
+    public enum DamageType { Slicing, Burning }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
